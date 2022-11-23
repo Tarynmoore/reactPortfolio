860557941f976963973d401cff735378d70e142f
Taryn Moore <tarynmoore628@gmail.com>
2022-11-23 16:46:00 -0700

    complete remodel. Got the homepage redone and need to weed through the css section.

--- portfolio/src/components/Container.js
+++ portfolio/src/components/Container.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 import NavTabs from './NavTabs';
-import AboutMe from './pages/AboutMe'
+import Home from './pages/Home'
 import Contact from './pages/Contact';
 import Portfolio from './pages/Portfolio';
 import Resume from './pages/Resume';
 
 export default function Container() {
-  const [currentPage, setCurrentPage] = useState('AboutMe');
+  const [currentPage, setCurrentPage] = useState('Home');
 
   const renderPage = () => {
-    if (currentPage === 'AboutMe') {
-      return <AboutMe />;
+    if (currentPage === 'Home') {
+      return <Home />;
     }
     if (currentPage === 'Contact') {
       return <Contact />;
--- portfolio/src/components/pages/Resume.js
+++ portfolio/src/components/pages/Resume.js
@@ -1,37 +1,37 @@
-import React from "react";
-import 'bootstrap/dist/css/bootstrap.css'
-import Row from 'react-bootstrap/Row';
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import pdf from './image/resume22.pdf'
+// import React from "react";
+// import 'bootstrap/dist/css/bootstrap.css'
+// import Row from 'react-bootstrap/Row';
+// import Container from 'react-bootstrap/Container';
+// import Col from 'react-bootstrap/Col';
+// import pdf from './image/resume22.pdf'
 
-export default function Resume() {
-    return(
-        <div>
-         <Row xs={12}>
-            <h1 className="line name">Want to know more?</h1>
-        </Row>
+// export default function Resume() {
+//     return(
+//         <div>
+//          <Row xs={12}>
+//             <h1 className="line name">Want to know more?</h1>
+//         </Row>
 
-        <Container>
-                <Row>
-                <Col xs={12}>
-                    <h3 >Languages I have mastered</h3>
-                    <h5>Frontend:</h5>
-                    <p>HTML, CSS, React, Javascript, jQuery</p>
-                    <h5>Backend:</h5>
-                    <p>Express, NoSQL, mySQL, NOde.js, MongoDb</p>
-                </Col>
-                <Col>
-                <a href={pdf} download>Download my resume!</a>
-                </Col>
-                </Row>
+//         <Container>
+//                 <Row>
+//                 <Col xs={12}>
+//                     <h3 >Languages I have mastered</h3>
+//                     <h5>Frontend:</h5>
+//                     <p>HTML, CSS, React, Javascript, jQuery</p>
+//                     <h5>Backend:</h5>
+//                     <p>Express, NoSQL, mySQL, NOde.js, MongoDb</p>
+//                 </Col>
+//                 <Col>
+//                 <a href={pdf} download>Download my resume!</a>
+//                 </Col>
+//                 </Row>
 
-        </Container>
-
-
+//         </Container>
 
 
 
-        </div>
-    )
-}
+
+
+//         </div>
+//     )
+// }
